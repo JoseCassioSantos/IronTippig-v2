2cba6f3c0aa787f237c450ef342979b66a3ce1dd
jose cassio dos santos <88626989+JoseCassioSantos@users.noreply.github.com>
2021-12-10 13:49:04 -0300

    semifinalizado

---
+++ script/over.js
@@ -0,0 +1,11 @@
+function overOn() {
+    document.getElementById("overlay").style.display = "block";
+    
+
+  }
+  
+  function overOff() {
+    document.getElementById("overlay").style.display = "none";
+    
+    
+  }
---
+++ script/tips.js
@@ -0,0 +1,44 @@
+function tip1() {
+  
+    let copyText = document.getElementById("tip1");
+  
+    
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); 
+  
+  
+    navigator.clipboard.writeText(copyText.value);
+    
+    window.alert("Copiado")
+    
+  }
+
+  function tip2() {
+  
+    let copyText = document.getElementById("tip2");
+  
+    
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); 
+  
+  
+    navigator.clipboard.writeText(copyText.value);
+    
+    window.alert("Copiado")
+    
+  }
+
+  function tip3() {
+  
+    let copyText = document.getElementById("tip3");
+  
+    
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); 
+  
+  
+    navigator.clipboard.writeText(copyText.value);
+    
+    window.alert("Copiado")
+    
+  }
