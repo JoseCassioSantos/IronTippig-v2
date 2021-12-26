a1e23897af81428b1ef737e21c34dae100d84676
jose cassio dos santos <88626989+JoseCassioSantos@users.noreply.github.com>
2021-12-26 10:54:43 -0300

    Multinotes att

---
+++ script/nota0.js
@@ -0,0 +1,83 @@
+function salvar(){
+    let txtnotas = document.getElementById('notes')
+    let tituloN = document.getElementById('titulo')
+  
+    
+     titulo = tituloN.value 
+     nota = txtnotas.value 
+    
+    
+     localStorage.setItem(titulo, nota)
+  
+     document.location.reload(true)
+    
+  }
+  
+  function apaga(){
+    let txtnotas = document.getElementById('notes')
+    let tituloN = document.getElementById('titulo')
+  
+    
+     titulo = tituloN.value 
+     nota = txtnotas.value 
+    
+    
+     localStorage.removeItem(titulo, nota)
+  
+     document.location.reload(true)
+    
+  }
+  
+  
+  
+  function backlist(x){
+    let rnota = document.getElementById('notes')
+    let rtitulo = document.getElementById('titulo')
+  
+    rtitulo.innerHTML = localStorage.key(x)
+    rnota.innerHTML = localStorage.getItem(localStorage.key(x))
+  
+    
+    
+  }
+  
+  onload= function() {
+  
+  
+  
+    lista = localStorage.length
+  
+    for(let i = 0; i < lista; i++ ){
+  
+      let name = localStorage.key(i)
+      let onclick = document.createAttribute('onclick')
+      let sclass = document.createAttribute('class')
+      let btn = document.createElement('button')
+      onclick.value ="backlist("+i+")"
+      sclass.value = "btn"
+      btn.innerHTML = name
+      btn.setAttributeNode(sclass)
+      btn.setAttributeNode(onclick)
+      document.getElementById('minhaLista').appendChild(btn)
+     
+    }
+    
+  
+    
+   
+  }
+  
+  function copy() {
+  
+    var copyText = document.getElementById("notes");
+  
+    
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); 
+  
+  
+    navigator.clipboard.writeText(copyText.value);
+    
+    
+  }
+  
